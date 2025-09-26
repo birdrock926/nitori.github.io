389c5cfa78be9e076b4badbb0478c27914912f70
birdrock926 <100780228+birdrock926@users.noreply.github.com>
2025-09-26 13:38:16 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>nitori.blog</title>
+    <style>
+        body { font-family: sans-serif; text-align: center; margin-top: 50px; }
+    </style>
+</head>
+<body>
+    <h1>ようこそ！</h1>
+    <p>こちらは nitori.blog のウェブサイトです。</p>
+    <p>現在、準備を進めています。</p>
+</body>
+</html>
+
+
