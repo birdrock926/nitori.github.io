943babb96652eb608559af043f3822e1acd8a81a
birdrock926 <100780228+birdrock926@users.noreply.github.com>
2025-10-07 18:23:05 +0900

    Improve Remark42 fallback handling

---
+++ cms/config/database.js
@@ -0,0 +1,31 @@
+export default ({ env }) => ({
+  connection: {
+    client: env('DATABASE_CLIENT', 'sqlite'),
+    connection:
+      env('DATABASE_CLIENT', 'sqlite') === 'sqlite'
+        ? {
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          }
+        : {
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'strapi'),
+            user: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'strapi'),
+            schema: env('DATABASE_SCHEMA', 'public'),
+            ssl: env.bool('DATABASE_SSL', false)
+              ? { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) }
+              : false,
+          },
+    pool: env('DATABASE_CLIENT', 'sqlite') === 'sqlite'
+      ? {
+          min: 0,
+          max: 1,
+          acquireTimeoutMillis: 60000,
+        }
+      : {
+          min: 2,
+          max: 10,
+        },
+  },
+});
